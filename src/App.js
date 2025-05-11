a6da509a52d81d04d6902b7e35da254ddd4f8aa8
Fenet Firomsa <fenetfiromsa2012@gmail.com>
2025-05-11 00:57:54 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Weather from './thefolder/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
